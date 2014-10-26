7c1f46d756de892f1e0154e6de58909e65f4165a
Justin B <justin.d.baldwin91@gmail.com>
2014-10-26 05:20:21 -0400

    Added connection and cause properties to base Event class

--- src/main/groovy/com/github/b2ojustin/irclibrary/event/Event.groovy
+++ src/main/groovy/com/github/b2ojustin/irclibrary/event/Event.groovy
@@ -1,5 +1,9 @@
 package com.github.b2ojustin.irclibrary.event
 
+import com.github.b2ojustin.irclibrary.IRCConnection
+
 
 class Event {
+    final IRCConnection connection
+    final String cause
 }
