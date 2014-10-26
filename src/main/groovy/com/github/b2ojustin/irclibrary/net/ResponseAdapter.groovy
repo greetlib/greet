38af8010a5a5d3f6da1f04854f58dd0c2d966803
Justin B <justin.d.baldwin91@gmail.com>
2014-10-26 05:19:30 -0400

    Log when empty string is returned from decoder.

--- src/main/groovy/com/github/b2ojustin/irclibrary/net/ResponseAdapter.groovy
+++ src/main/groovy/com/github/b2ojustin/irclibrary/net/ResponseAdapter.groovy
@@ -1,13 +1,9 @@
 package com.github.b2ojustin.irclibrary.net
 
 import com.github.b2ojustin.irclibrary.IRCConnection
-import com.github.b2ojustin.irclibrary.event.ConnectionEvent
-import com.github.b2ojustin.irclibrary.exception.InvalidServerResponse
 import groovy.util.logging.Log4j2
-import io.netty.buffer.ByteBuf
 import io.netty.channel.ChannelHandlerAdapter
 import io.netty.channel.ChannelHandlerContext
-import io.netty.channel.ChannelPromise
 
 @Log4j2
 class ResponseAdapter extends ChannelHandlerAdapter {
@@ -19,11 +15,6 @@ class ResponseAdapter extends ChannelHandlerAdapter {
     }
 
     @Override
-    public void handlerAdded(ChannelHandlerContext ctx) {
-        log.info "${this.class.name} added to channel pipeline."
-    }
-
-    @Override
     void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         log.error cause.getMessage(), cause
         ctx.close()
@@ -32,15 +23,19 @@ class ResponseAdapter extends ChannelHandlerAdapter {
     @Override
     void channelRead(ChannelHandlerContext ctx, Object msg) {
         msg = msg as String
+        if(msg.isEmpty()) {
+            log.warn "Received empty string from decoder."
+            ctx.fireChannelRead(msg)
+            return
+        }
         List<String> parts = msg.split(":")
         List<String> cmdParts = parts[1].split(" ")
         String trail = parts[2]
-        String[] params;
+        String[] params = [];
         if(cmdParts.size() > 2) params = cmdParts.subList(2, cmdParts.size())
 
         ServerResponse r = [
                 rawData: msg,
-                remoteAddress: ctx.channel().remoteAddress(),
                 source: cmdParts[0],
                 command: cmdParts[1],
                 params: params,
