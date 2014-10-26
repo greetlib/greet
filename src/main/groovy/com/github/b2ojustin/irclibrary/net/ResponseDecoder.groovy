623667390fd6d9ad6a4cff7ae2e3f201c4af0139
Justin B <justin.d.baldwin91@gmail.com>
2014-10-26 05:18:49 -0400

    Fixed NPE during fragmentation

--- src/main/groovy/com/github/b2ojustin/irclibrary/net/ResponseDecoder.groovy
+++ src/main/groovy/com/github/b2ojustin/irclibrary/net/ResponseDecoder.groovy
@@ -2,9 +2,7 @@ package com.github.b2ojustin.irclibrary.net
 
 import groovy.util.logging.Log4j2
 import io.netty.buffer.ByteBuf
-import io.netty.buffer.Unpooled
 import io.netty.channel.ChannelHandlerContext
-import io.netty.handler.codec.ByteToMessageDecoder
 import io.netty.handler.codec.LineBasedFrameDecoder
 
 @Log4j2
@@ -17,7 +15,7 @@ class ResponseDecoder extends LineBasedFrameDecoder {
     @Override
     protected Object decode(ChannelHandlerContext ctx, ByteBuf buffer) throws Exception {
         buffer = super.decode(ctx, buffer) as ByteBuf
-        if(buffer.readableBytes() == 0) return ""
+        if(buffer == null) return ""
         else {
             byte[] data = new byte[buffer.readableBytes()]
             buffer.readBytes(data)
