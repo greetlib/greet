cdaaf466eb3904b1a8573867dc36925ff7e018bc
Justin42 <justin.d.baldwin91@gmail.com>
2014-10-28 05:17:49 -0400

    Gradle/Groovy doesn't seem to like @Ignore JUnit annotation

--- src/test/groovy/io/github/greetlib/greet/IRCConnectionTest.groovy
+++ src/test/groovy/io/github/greetlib/greet/IRCConnectionTest.groovy
@@ -1,14 +1,23 @@
 package io.github.greetlib.greet
 
-import io.github.greetlib.greet.IRCConnection
+import io.github.greetlib.greet.net.UserInfo
+import org.junit.Before
 import org.junit.Ignore
 import org.junit.Test
 
 class IRCConnectionTest extends GroovyTestCase {
-    IRCConnection conn = new IRCConnection()
+    IRCConnection conn
 
-    @Ignore
-    @Test
+    @Before
+    void setUp() {
+        UserInfo userInfo = [
+              userName: "Test",
+              nickName: "Test2738",
+              realName: "Testing",
+        ]
+        conn = new IRCConnection(userInfo)
+    }
+
     void testConnect() {
         assertTrue "Unable to connect", conn.connect("chat.freenode.net", 6667).sync().success
     }
